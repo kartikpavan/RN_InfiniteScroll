12e7cba2ceb5e6d8ae42dd671db7e7f42f3625ce
Pavan <kartik.varanasi@yahoo.com>
2024-02-02 13:43:52 +0530

    Basic Pagination

--- components/MyList.tsx
+++ components/MyList.tsx
@@ -1,38 +1,51 @@
-import { ActivityIndicator, FlatList } from "react-native";
+import { ActivityIndicator, FlatList, Text, View } from "react-native";
 import SingleCharacter from "./SingleCharacter";
 import { useEffect, useState } from "react";
 
 const MyList = () => {
-  const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [items, setItems] = useState<any[]>([]);
+  const [nextPageURL, setNextPageURL] = useState<string>(
+    "https://rickandmortyapi.com/api/character"
+  );
+
+  const fetchItems = async () => {
+    try {
+      setIsLoading(true);
+      const response = await fetch(nextPageURL);
+      const data = await response.json();
+      setItems((prev) => {
+        return [...prev, ...data.results];
+      });
+      setNextPageURL(data.info.next);
+    } catch (error) {
+      if (error instanceof Error) console.log(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   useEffect(() => {
-    const fetchItems = async () => {
-      try {
-        setIsLoading(true);
-        const response = await fetch(
-          "https://rickandmortyapi.com/api/character"
-        );
-        const data = await response.json();
-        setItems(data.results);
-      } catch (error) {
-        if (error instanceof Error) console.log(error.message);
-      } finally {
-        setIsLoading(false);
-      }
-    };
+    // On mount ,fetch items
     fetchItems();
   }, []);
-
-  if (isLoading) {
-    return <ActivityIndicator />;
-  }
 
   return (
     <FlatList
       data={items}
       renderItem={({ item, index }) => <SingleCharacter character={item} />}
       contentContainerStyle={{ gap: 10 }}
+      ListFooterComponent={() => (
+        <View>
+          {isLoading && <ActivityIndicator />}
+          <Text
+            onPress={fetchItems} // on press fetch More items
+            style={{ textAlign: "center", fontSize: 20, color: "green" }}
+          >
+            Load More
+          </Text>
+        </View>
+      )}
     />
   );
 };
